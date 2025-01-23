1778aaa681070efe3a152b0d3c0990881f25b00c
xavier evans <evans.xavier@ymail.com>
2025-01-23 14:32:49 +0000

    update cors exceptions in settings

--- src/paperless/settings.py
+++ src/paperless/settings.py
@@ -535,6 +535,8 @@ CSRF_TRUSTED_ORIGINS = __get_list("PAPERLESS_CSRF_TRUSTED_ORIGINS")
 CORS_ALLOWED_ORIGINS = __get_list(
     "PAPERLESS_CORS_ALLOWED_HOSTS",
     ["http://localhost:8000"],
+    ["http://localhost:3000"],
+    ["http://localhost:3001"],
 )
 
 if DEBUG:
